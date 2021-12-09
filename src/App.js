5fd6a571587416af08c8acf025f339f3a3837cff
Mathew Snyder <mgmsnyder@gmail.com>
2021-12-09 10:12:12 -0500

    Whoops I did the whole project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import headshot from './headshot.jpg'
 import './App.css';
+import Main from './Main'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Could it be anything?
+      <header className='App-header'>
+      <img id="headshot" src={headshot} alt='Mat with a baby on his head' />
       </header>
+      <Main />
     </div>
   );
 }
 
-export default App;
